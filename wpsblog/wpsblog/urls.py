7ba10cb1bb0398ed376b1eb2abf6495d9dd055fb
spreadmesh <spreadmesh@gmail.com>
2016-05-24 16:42:45 +0900

    Add home controller: returns "hello world" text

--- wpsblog/wpsblog/urls.py
+++ wpsblog/wpsblog/urls.py
@@ -16,6 +16,15 @@ Including another URLconf
 from django.conf.urls import url
 from django.contrib import admin
 
+from django.http.response import HttpResponse
+
+
+# MVC Controller
+def home(request):
+    return HttpResponse("hello world") 
+
+
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'^$', home),
 ]
