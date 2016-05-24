28e87b28028bb272b7f84bb4a3ab48550dbc3941
spreadmesh <spreadmesh@gmail.com>
2016-05-24 17:12:33 +0900

    refactor controller name to "views": django naming convention

--- wpsblog/wpsblog/controller.py
+++
@@ -1,9 +0,0 @@
-from django.http.response import HttpResponse
-
-
-def home(request):
-    return HttpResponse("hello world")
-
-def room(request, room_id):
-    return HttpResponse("This is a room detail " + room_id)
-
--- wpsblog/wpsblog/views.py
+++ wpsblog/wpsblog/views.py
@@ -5,5 +5,6 @@ def home(request):
     return HttpResponse("hello world")
 
 def room(request, room_id):
-    return HttpResponse("This is a room detail " + room_id)
-
+    zigbang = "https://api.zigbang.com/v1/items?detail=true&item_ids=" + str(room_id)
+#    return HttpResponse("This is a room detail " + room_id)
+    return HttpResponse(zigbang)
