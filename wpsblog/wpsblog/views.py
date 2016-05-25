7a2c654408f9b7947fedac0a1919550a1d9d8120
spreadmesh <spreadmesh@gmail.com>
2016-05-25 16:51:42 +0900

    update home and news view: use django template loader and engine

--- wpsblog/wpsblog/views.py
+++ wpsblog/wpsblog/views.py
@@ -2,14 +2,17 @@ import json
 import requests
 
 from django.http.response import HttpResponse
-from django.conf import settings
-
-from wpsblog.renderer import render
-
+from django.template import loader
 
 
 def home(request):
-    return render("home", {"site_name": "wps_blog"})
+    template = loader.get_template("home.html")
+    return HttpResponse(
+        template.render(
+            {"site_name": "wps blog"},
+            request,
+        )
+    )
 
 
 def room(request, room_id):
@@ -34,19 +37,10 @@ def news(request):
             news_list,
         ))
 
-        
-    count = len(news_list)
-    news_content = "".join([
-        "<h2>{title}</h2><img src={image_src}><p>{content}</p>".format(
-            title=news.get('title'),
-            image_src=news.get('image'),
-            content=news.get('content'),
+    template = loader.get_template("news.html")
+    return HttpResponse(
+            template.render(
+                {"news_list": news_list},
+                request,
+            )
         )
-        for news
-        in news_list
-    ])
-
-    return render("news", {
-        "count": str(count),
-        "news_content": news_content,
-    })
