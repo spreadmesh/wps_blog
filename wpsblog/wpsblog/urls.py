68e358aa6897086765a4ae5ab24d0e86a05cdcc8
spreadmesh <spreadmesh@gmail.com>
2016-05-30 14:58:26 +0900

    refactor urls.py: group policy urls using include, and namespace

--- wpsblog/wpsblog/urls.py
+++ wpsblog/wpsblog/urls.py
@@ -1,4 +1,4 @@
-from django.conf.urls import url
+from django.conf.urls import url, include
 from django.contrib import admin
 
 from wpsblog.views import *
@@ -12,7 +12,13 @@ urlpatterns = [
     url(r'^news/$', news, name="news"),
     url(r'^about/us/$', about, name="about"),
     
-    url(r'^policy/terms/$', terms, name="terms"),
-    url(r'^policy/privacy/$', privacy, name="privacy"),
-    url(r'^policy/disclaimer/$', disclaimer, name="disclaimer"),
+    # url(r'^policy/terms/$', terms, name="terms"),
+    # url(r'^policy/privacy/$', privacy, name="privacy"),
+    # url(r'^policy/disclaimer/$', disclaimer, name="disclaimer"),
+    
+    url(r'^policy/', include([
+        url(r'terms/$', terms, name="terms"),
+        url(r'privacy/$', privacy, name="privacy"),
+        url(r'disclaimer/$', disclaimer, name="disclaimer"),
+    ], namespace="policy")),
 ]
