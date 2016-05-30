4425626d6374627e1e1a07ac8c175109091682a4
spreadmesh <spreadmesh@gmail.com>
2016-05-30 14:50:53 +0900

    update urls.py: add policy before policies

--- wpsblog/wpsblog/urls.py
+++ wpsblog/wpsblog/urls.py
@@ -11,7 +11,8 @@ urlpatterns = [
     url(r'^rooms/(?P<room_id>\d+)/$', room, name="room"),
     url(r'^news/$', news, name="news"),
     url(r'^about/us/$', about, name="about"),
-    url(r'^terms/$', terms, name="terms"),
-    url(r'^privacy/$', privacy, name="privacy"),
-    url(r'^disclaimer/$', disclaimer, name="disclaimer"),
+    
+    url(r'^policy/terms/$', terms, name="terms"),
+    url(r'^policy/privacy/$', privacy, name="privacy"),
+    url(r'^policy/disclaimer/$', disclaimer, name="disclaimer"),
 ]
