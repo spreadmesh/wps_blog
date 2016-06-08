fb7b412d463718d73e4322fa9a37439249c7cf62
gruteogisu <gruteogisu@gmail.com>
2016-06-08 21:51:05 +0900

    init mypage

--- wpsblog/wpsblog/urls/auth.py
+++ wpsblog/wpsblog/urls/auth.py
@@ -7,4 +7,5 @@ urlpatterns = [
     url(r'^login/$', login, name="login"),
     url(r'^signup/$', signup, name="signup"),
     url(r'^logout/$', logout, name="logout"),
+    url(r'^mypage/$', mypage, name="mypage"),
 ]
